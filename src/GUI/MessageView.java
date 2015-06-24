00dadc3da73b2e2c028e4fd22d1f181f2b019dea
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-06-24 12:11:59 +0200

    Added dates to message design

--- src/GUI/MessageView.java
+++ src/GUI/MessageView.java
@@ -8,6 +8,7 @@ package GUI;
 import Model.Message;
 import java.awt.Color;
 import javax.swing.BorderFactory;
+import javax.swing.JScrollBar;
 
 /**
  *
@@ -31,7 +32,7 @@ public class MessageView extends javax.swing.JPanel {
             this.setBackground(new Color(0x00FFFF));
         }
         else{
-            this.setBackground(new Color(0xF0F0F0));
+            this.setBackground(new Color(0xCCCCCC));
         }
     }
     
@@ -67,6 +68,12 @@ public class MessageView extends javax.swing.JPanel {
         select(false);
         this.messageModel = m;
         this.labelText.setText(messageModel.toString());
+        this.labelDate.setText((m.getDate() == null)?"--ERR_DATE--":Message.getDateFormat().format(messageModel.getDate()));
+        
+        //jScrollPane2.validate();
+        //JScrollBar vertical = jScrollPane2.getVerticalScrollBar();
+        //vertical.setValue( vertical.getMinimum() );
+        this.labelText.setCaretPosition(0);
         repaint();
     }
     
@@ -85,12 +92,13 @@ public class MessageView extends javax.swing.JPanel {
 
         jScrollPane2 = new javax.swing.JScrollPane();
         labelText = new javax.swing.JEditorPane();
+        labelDate = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(255, 255, 255));
+        setBackground(new java.awt.Color(204, 204, 204));
         setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        setMaximumSize(new java.awt.Dimension(675, 75));
-        setMinimumSize(new java.awt.Dimension(675, 75));
-        setPreferredSize(new java.awt.Dimension(675, 75));
+        setMaximumSize(new java.awt.Dimension(690, 93));
+        setMinimumSize(new java.awt.Dimension(690, 93));
+        setPreferredSize(new java.awt.Dimension(690, 93));
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 formMouseClicked(evt);
@@ -105,18 +113,27 @@ public class MessageView extends javax.swing.JPanel {
         labelText.setMinimumSize(new java.awt.Dimension(675, 14));
         jScrollPane2.setViewportView(labelText);
 
+        labelDate.setBackground(new java.awt.Color(255, 255, 255));
+        labelDate.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        labelDate.setForeground(new java.awt.Color(0, 0, 255));
+        labelDate.setText("24/06/2015 11:30:00");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 686, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 666, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(labelDate, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 71, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(labelDate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -127,6 +144,7 @@ public class MessageView extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel labelDate;
     private javax.swing.JEditorPane labelText;
     // End of variables declaration//GEN-END:variables
 }
