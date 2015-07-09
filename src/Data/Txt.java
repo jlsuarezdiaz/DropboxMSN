a9e40ac4a1e1ceaec642ffef0443096e8bab030a
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-07-09 20:17:36 +0200

    Program texts.

---
+++ src/Data/Txt.java
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author: Juan Luis Suarez Diaz
+// Jun, 2015
+// Dropbox MSN
+////////////////////////////////////////////////////////////////////////////////
+package Data;
+
+/**
+ * Class Txt.
+ * Contains all remarkable texts related to the program.
+ * @author Juan Luis
+ */
+public class Txt {
+
+    public static final String OLD_VERSIONS_INFO =
+            "Versión 1.0 (15/6/2015):"+
+            "\n- Primer programa. Funcionalidades básicas del messenger e interfaz gráfica.\n\n"+
+            "Versión 1.1 (17/6/2015):"+
+            "\n- Añadido sonido al recibir un mensaje."+
+            "\n- Resuelto el problema del scroll."+
+            "\n- Resuelto el conflicto con la actualización del usuario."+
+            "\n- Mejora en el ajuste del nombre de usuario.\n\n"+
+            "Versión 1.2 (24/6/2015):"+
+            "\n- Mejora en el manejo de los usuarios."+
+            "\n- Añadida selección de mensajes (sin ningún uso por el momento)."+
+            "\n- Añadidas fechas a los mensajes.\n\n"+
+            "Versión 1.3 (5/7/2015):"+
+            "\n- Añadidos colores al menú de estados de usuario."+
+            "\n- Añadida la opción de enviar mensajes pulsando Intro.\n\n"+
+            "Versión 1.4 (6/7/2015):"+
+            "\n- Añadidas las opciones de copiar, pegar y borrar los mensajes seleccionados."+
+            "\n- Ahora los mensajes copiados se añaden al portapapeles del sistema."+
+            "\n- Añadido el menú de configuración.\n\n"
+            ;
+    
+    public static final String LAST_VERSION_INFO =
+            "Versión 2.0 (9/7/2015):"+
+            "\n- Mejoras al enviar mensajes pulsando Intro."+
+            "\n- Mejoras al habilitar el menú de configuración."+
+            "\n- Añadida la opción de guardar mensajes."+
+            "\n- Añadidos al programa los menús acerca del programa.\n\n"
+    ;
+    
+    public static final String AUTHOR = "Juan Luis Suárez Díaz";
+    
+    public static final String VERSION = "v2.0";
+    
+    public static final String COPYRIGHT = "© 2015";
+    
+    public static final String EDITION = "Dropbox MSN Ultimate Java Edition";
+    
+    public static final String PROGRAM_INFO = "¿Buscas un servicio de mensajería divertido, eficaz y sencillo?\n"+
+            "Con el nuevo Dropbox MSN, tendrás eso y mucho más.\nInvita a tus amigos, introduce un nombre, ¡y empieza a chatear!";
+}
