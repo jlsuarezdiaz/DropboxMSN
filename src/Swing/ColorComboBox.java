ad264256d1c80ca745977f2c9cddc3dc26151082
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-07-11 17:22:21 +0200

    Added comments.

--- src/Swing/ColorComboBox.java
+++ src/Swing/ColorComboBox.java
@@ -9,17 +9,42 @@ import java.awt.Color;
 import java.awt.Component;
 import javax.swing.*;
 
-
+/**
+ * Class ColorComboBox.
+ * Renderer for User State ComboBox.
+ * @author Juan Luis
+ */
 public class ColorComboBox extends JPanel implements ListCellRenderer
 {
-
+    /**
+     * serialVersionUID
+     */
     private static final long serialVersionUID = -1L;
+    
+    /**
+     * Array with the colors.
+     */
     private Color[] colors;
+    
+    /**
+     * Array with the strings.
+     */
     private String[] strings;
-
+    
+    /**
+     * Text panel.
+     */
     JPanel textPanel;
+    
+    /**
+     * Text.
+     */
     JLabel text;
 
+    /**
+     * Constructor.
+     * @param combo ComboBox. 
+     */
     public ColorComboBox(JComboBox combo) {
 
         textPanel = new JPanel();
@@ -30,26 +55,51 @@ public class ColorComboBox extends JPanel implements ListCellRenderer
         textPanel.add(text);
     }
 
+    /**
+     * Set colors array.
+     * @param col Array with the colors.
+     */
     public void setColors(Color[] col)
     {
         colors = col;
     }
 
+    /**
+     * Set strings array.
+     * @param str Array with the strings.
+     */
     public void setStrings(String[] str)
     {
         strings = str;
     }
 
+    /**
+     * Get colors array.
+     * @return Colors array. 
+     */
     public Color[] getColors()
     {
         return colors;
     }
 
+    /**
+     * Get strings array.
+     * @return Strings array.
+     */
     public String[] getStrings()
     {
         return strings;
     }
 
+    /**
+     * Get List Cell Renderer Component.
+     * @param list List.
+     * @param value Component value.
+     * @param index Component index.
+     * @param isSelected Indicates if component is selected.
+     * @param cellHasFocus Indicates if component is focused.
+     * @return Component.
+     */
     @Override
     public Component getListCellRendererComponent(JList list, Object value,
             int index, boolean isSelected, boolean cellHasFocus) {
